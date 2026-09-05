0eaeb57eac28b4e852e132cf4b1683943aeac37b
BatThommy <batthommy@gmail.com>
2026-09-05 14:53:43 +0200

    renamed items and removed unnecessary files

---
+++ VBS/INDEX/script.js
@@ -0,0 +1,6 @@
+//!! JS still to be done !!
+
+/* 
+    !! Like linking numbers and projects to actual sources, variables, and arrays, 
+    !! tweaking a few things, fixing button connections, etc.
+*/
