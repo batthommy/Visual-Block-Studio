fa5add575199a91adb8262f7f6a64b909b8d5226
BatThommy <batthommy@gmail.com>
2026-09-05 14:52:55 +0200

    Delete VBS directory

--- VBS/INDEX/script.js
+++
@@ -1,6 +0,0 @@
-//!! JS still to be done !!
-
-/* 
-    !! Like linking numbers and projects to actual sources, variables, and arrays, 
-    !! tweaking a few things, fixing button connections, etc.
-*/
